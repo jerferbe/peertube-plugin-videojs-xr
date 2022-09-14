a6bca690aa96779caed3a02f1b680e9f60e951f2
FERRIER Jerome <jerome.ferrier@ext.europarl.europa.eu>
2022-09-14 15:46:20 +0200

    mediainfo.projection compliant

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PeerTube plugin videojs-vr
 
-See https://docs.joinpeertube.org/#/contribute-plugins?id=write-a-plugintheme
+PeerTube plugin to play 360 content. 
+
+Install the plugin on your instance then enable it per video using the 360 tag. If you need specific projection (available from https://github.com/videojs/videojs-vr#projection) then use 360:projection_name. The plugin has been tested with a Ricoh Theta V, Ricoh Theta Z1 and Insta360Pro with different resolution and bitrates. 
+
+Do make sure that your 360 camera does provide an audio source.
--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,103 +1,129 @@
- // video-vr.js module
- //  import videojs from 'video.js';
- //  import 'videojs-vr/dist/videojs-vr.css';
- //  import 'videojs-vr/dist/videojs-vr.min.js';
- var vjsPlayer;
+let vjsPlayer;
+let scripts;
 
- function register({ registerHook, peertubeHelpers }) {
-     loadJS('https://videojs-vr.netlify.app/node_modules/video.js/dist/video.js', videojsLoaded, document.head);
+function register({ registerHook, peertubeHelpers }) {
+    init(registerHook, peertubeHelpers)
+        .catch(err => console.error('Cannot init', err))
+}
 
-     init(registerHook, peertubeHelpers)
-         .catch(err => console.error('Cannot init', err))
+export {
+    register
+}
 
- }
+function init(registerHook, peertubeHelpers) {
+    return peertubeHelpers.getSettings()
+        .then(s => {
+            const { notifier } = peertubeHelpers;
 
+            scripts = Array
+                .from(document.querySelectorAll('script'))
+                .map(scr => scr.src);
 
- export {
-     register
- }
+            if (!scripts.includes('https://cdn.jsdelivr.net/npm/video.js@7.7.4/dist/video.min.js')) {
+                loadJS('https://cdn.jsdelivr.net/npm/video.js@7.7.4/dist/video.min.js', videojsLoaded, document.head);
+            } else {
+                console.log('videojs already loaded');
+            }
+            console.log('scripts :>> ', scripts);
 
+            registerHook({
+                target: 'action:video-watch.video.loaded',
+                handler: ({ video }) => {
+                    //  console.log('video hooked info :>> ', video);
+                }
+            })
 
- function init(registerHook, peertubeHelpers) {
-     return peertubeHelpers.getSettings()
-         .then(s => {
+            registerHook({
+                target: "action:video-watch.player.loaded",
+                handler: ({ player, videojs, video }) => {
 
-             // console.log('peertubeHelpers settings --->', s, s['watermark-image-url']);
+                    //  console.log('peertube hooked info :>> ', player, videojs, video);
+                    console.log('video.tags :>> ', video.tags);
 
-             registerHook({
-                 target: 'action:video-watch.video.loaded',
-                 handler: () => {
-                     // displayHelloWorld()
-                     // displayMediaProjection();
-                 }
-             })
-             const { notifier } = peertubeHelpers
+                    const compliant360 = video.tags.find(element => {
+                        if (element.includes('360:')) {
+                            return true;
+                        }
+                    });
 
-             registerHook({
-                 target: "action:video-watch.player.loaded",
-                 handler: ({ player, videojs, video }) => {
+                    player.player_.pause();
 
-                     console.log('peertube hooked info :>> ', player, videojs, video);
-                     vjsPlayer = videojs(document.querySelector('.vjs-tech'), { autoplay: false });
+                    if (compliant360 !== undefined) {
 
-                     if (video.tags.includes('360')) {
+                        peertubeHelpers.showModal({
+                            title: 'Interactive video',
+                            content: '<p>This seems to be a 360° video.</p><p>-Click and drag your mouse to interact with the video</p><p>OR</p><p>-Move your head if you have a VR headset.</p>',
+                            close: true,
+                            //  cancel: { value: 'cancel', action: () => {} },
+                            confirm: {
+                                value: 'Ok',
+                                action: () => {
+                                    goForVrPlayer(notifier, video);
+                                }
+                            },
+                        })
+                    }
+                }
+            })
+        })
+}
 
-                         peertubeHelpers.showModal({
-                             title: 'VR interactive video',
-                             content: '<p>Hoow this seems to be a 360-degrees video.</p><p>Click and drag your mouse to interact with the video.</p>',
-                             close: true,
-                             //  cancel: { value: 'cancel', action: () => {} },
-                             confirm: {
-                                 value: 'Ok',
-                                 action: () => {
-                                     goForVrPlayer(notifier);
-                                 }
-                             },
-                         })
-                     }
+var loadJS = function(url, implementationCode, location) {
+    var scriptTag = document.createElement('script');
+    scriptTag.src = url;
+    scriptTag.onload = implementationCode;
+    scriptTag.onreadystatechange = implementationCode;
+    location.appendChild(scriptTag);
+};
 
+var videojsLoaded = function() {
+    console.log('videojs is loaded');
+    if (!scripts.includes('https://cdn.jsdelivr.net/npm/videojs-vr@1.7.2/dist/videojs-vr.min.js')) {
+        loadJS('https://cdn.jsdelivr.net/npm/videojs-vr@1.7.2/dist/videojs-vr.min.js', videojsvrLoaded, document.head);
+    } else {
+        console.log('videojsVR already loaded');
+    }
+}
 
+var videojsvrLoaded = function() {
+    console.log('videojs-vr is loaded');
+}
 
+var goForVrPlayer = function(notifier, video) {
 
-                 }
-             })
-         })
- }
+    vjsPlayer = videojs(document.querySelector('.video-js video'));
+    vjsPlayer.mediainfo = vjsPlayer.mediainfo || {};
 
- var loadJS = function(url, implementationCode, location) {
-     var scriptTag = document.createElement('script');
-     scriptTag.src = url;
-     scriptTag.onload = implementationCode;
-     scriptTag.onreadystatechange = implementationCode;
-     location.appendChild(scriptTag);
- };
+    if (video.tags.includes('360:180')) {
+        vjsPlayer.mediainfo.projection = '180';
+    } else if (video.tags.includes('360:180_LR')) {
+        vjsPlayer.mediainfo.projection = '180_LR';
+    } else if (video.tags.includes('360:180_MONO')) {
+        vjsPlayer.mediainfo.projection = '180_MONO';
+    } else if (video.tags.includes('360:180_LR')) {
+        vjsPlayer.mediainfo.projection = '180_LR';
+    } else if (video.tags.includes('360:360')) {
+        vjsPlayer.mediainfo.projection = '360';
+    } else if (video.tags.includes('360:Cube')) {
+        vjsPlayer.mediainfo.projection = 'Cube';
+    } else if (video.tags.includes('360:360_CUBE')) {
+        vjsPlayer.mediainfo.projection = '360_CUBE';
+    } else if (video.tags.includes('360:360_LR')) {
+        vjsPlayer.mediainfo.projection = '360_LR';
+    } else if (video.tags.includes('360:360_TB')) {
+        vjsPlayer.mediainfo.projection = '360_TB';
+    } else if (video.tags.includes('360:EAC')) {
+        vjsPlayer.mediainfo.projection = 'EAC';
+    } else if (video.tags.includes('360:EAC_LR')) {
+        vjsPlayer.mediainfo.projection = 'EAC_LR';
+    }
 
- var videojsLoaded = function() {
-     console.log('videojs is loaded');
-     loadJS('https://videojs-vr.netlify.app/dist/videojs-vr.js', videojsvrLoaded, document.head);
- }
- var videojsvrLoaded = function() {
-     console.log('videojs-vr is loaded');
- }
- var goForVrPlayer = function(notifier) {
+    vjsPlayer.vr({ projection: 'AUTO', sphereDetail: 32, debug: true, forceCardboard: false });
 
-     //  vjsPlayer = videojs('vjs_video_3_html5_api');
-     vjsPlayer = videojs(document.querySelector('.vjs-tech'), { autoplay: false });
-     //  vjsPlayer.options.autoplay = false;
+    console.log('mediainfo.projection :>> ', vjsPlayer.mediainfo.projection);
 
-     vjsPlayer.vr({ projection: '360' });
+    vjsPlayer.play();
 
-     console.log('switch to videojs-vr Player');
-     notifier.success('Switch to VR Player')
-
-     //  console.log('projection type :>> ', vjsPlayer.mediainfo.projection);
-
-
- }
-
-
-
-
- function displayHelloWorld() {
-     console.info('Hellow..');
- }
+    console.log('switch to videojs-vr Player');
+    notifier.success('Switched to 360° video player')
+}
