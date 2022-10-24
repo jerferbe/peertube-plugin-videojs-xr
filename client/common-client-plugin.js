17d92651e53fb8a8ad5a6c04eda54d43c191da29
FERRIER Jerome <jerome.ferrier@ext.europarl.europa.eu>
2022-10-24 14:29:27 +0200

    remove force cardboard

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,6 +1,7 @@
 let scripts;
 let vjsPlayer;
 
+
 function register({ registerHook, peertubeHelpers }) {
     init(registerHook, peertubeHelpers)
         .catch(err => console.error('Cannot init', err))
@@ -24,12 +25,43 @@ function init(registerHook, peertubeHelpers) {
             } else {
                 console.log('videojs already loaded');
             }
+
             console.log('scripts :>> ', scripts);
 
+            registerHook({
+                target: 'action:router:navigation-end',
+                handler: () => {
+                    // Fired when router navigation-end
+                    console.log('router navigation-end');
+
+                }
+            })
+            registerHook({
+                target: 'action:embed.player.loaded',
+                handler: ({ videojs, video }) => {
+                    // Fired when the embed loaded the player
+                    console.log('loaded peertube embed player:>> ', video);
+
+                }
+            })
             registerHook({
                 target: 'action:video-watch.video.loaded',
                 handler: ({ videojs, video }) => {
                     // console.log('video loaded hooked info :>> ', videojs, video);
+
+                    let isCompliant360;
+                    isCompliant360 = video.tags.find(element => {
+                        if (element.includes('360:')) {
+                            return true;
+                        }
+                    });
+
+                    if (isCompliant360 !== undefined) {
+                        setTimeout(() => {
+                            goForVrPlayer(notifier, video);
+                        }, 1000);
+                    }
+
                 }
             })
 
@@ -40,48 +72,67 @@ function init(registerHook, peertubeHelpers) {
                     // console.log('player loaded hooked info :>> ', player, videojs, video);
                     console.log('video tags :>> ', video.tags);
 
-                    const compliant360 = video.tags.find(element => {
-                        if (element.includes('360:')) {
-                            return true;
-                        }
-                    });
+                    // vjsPlayer = videojs(document.querySelector('.video-js video'), {
+                    //     autoplay: false,
+                    //     posterImage: true,
+                    //     controls: true,
+                    //     controlBar: {
+                    //         'pictureInPictureToggle': false
+                    //     },
+                    // });
 
-                    if (compliant360 !== undefined) {
 
-                        vjsPlayer = videojs(document.querySelector('.video-js video'), {
-                            autoplay: false,
-                            posterImage: false,
-                            controls: false
-                        });
 
-                        vjsPlayer.ready(function() {
-                            var promise = vjsPlayer.play();
-                            if (promise !== undefined) {
-                                promise.then(function() {
-                                    // Autoplay started!
-                                    vjsPlayer.pause();
-                                    vjsPlayer.controls = false;
-                                }).catch(function(error) {
-                                    // Autoplay was prevented.
-                                    vjsPlayer.pause();
-                                    vjsPlayer.controls = false;
-                                });
-                            }
-                        });
 
-                        peertubeHelpers.showModal({
-                            title: 'This seems to be a 360° video',
-                            content: '<p>-Click and drag your mouse to interact with the video</p><p>OR</p><p>-Move your head if you have a VR headset.</p>',
-                            close: true,
-                            //  cancel: { value: 'cancel', action: () => {} },
-                            confirm: {
-                                value: 'Ok',
-                                action: () => {
-                                    goForVrPlayer(notifier, video);
-                                }
-                            },
-                        })
-                    }
+
+
+                    // vjsPlayer.reset();
+                    // vjsPlayer.controlBar.addChild('CardboardButton', {});
+
+                    // var vrDisplay = new CardboardVRDisplay();
+                    // var canvas = document.querySelector('canvas');
+
+                    // var myButton = vjsPlayer.controlBar.addChild('button', {});
+                    // var myButtonDom = myButton.el();
+                    // myButtonDom.innerHTML = '🥽';
+                    // myButtonDom.onclick = function() {
+                    //     // window.dispatchEvent(new window.Event('vrdisplayactivate'));
+                    //     alert('enter vr mode');
+                    //     window.addEventListener('vrdisplayactivate', function() {
+                    //         renderer.vr.getDevice().requestPresent([{ source: renderer.domElement }])
+                    //     });
+
+                    // };
+
+
+                    // vjsPlayer.ready(function() {
+                    //     var promise = vjsPlayer.play();
+                    //     if (promise !== undefined) {
+                    //         promise.then(function() {
+                    //             // Autoplay started!
+                    //             vjsPlayer.pause();
+                    //             vjsPlayer.controls = false;
+                    //         }).catch(function(error) {
+                    //             // Autoplay was prevented.
+                    //             vjsPlayer.pause();
+                    //             vjsPlayer.controls = false;
+                    //         });
+                    //     }
+                    // });
+
+                    // peertubeHelpers.showModal({
+                    //     title: 'This seems to be a 360° video',
+                    //     content: '<p>-Click and drag your mouse to interact with the video</p><p>OR</p><p>-Move your head if you have a VR headset.</p>',
+                    //     close: true,
+                    //     //  cancel: { value: 'cancel', action: () => {} },
+                    //     confirm: {
+                    //         value: 'Ok',
+                    //         action: () => {
+                    //             goForVrPlayer(notifier, video);
+                    //         }
+                    //     },
+                    // })
+                    // }
                 }
             })
         })
@@ -106,14 +157,43 @@ var videojsLoaded = function() {
 
 var videojsvrLoaded = function() {
     console.log('videojs-xr 0.1.0 is loaded');
+    if (!scripts.includes('https://cdn.jsdelivr.net/npm/webxr-polyfill@latest/build/webxr-polyfill.js')) {
+        loadJS('https://cdn.jsdelivr.net/npm/webxr-polyfill@latest/build/webxr-polyfill.js', webxrPolyfillLoaded, document.head);
+    } else {
+        console.log('webxr-polyfill already loaded');
+    }
+}
+
+var webxrPolyfillLoaded = function() {
+    console.log('webxr-polyfill is loaded');
 }
 
 var goForVrPlayer = function(notifier, video) {
 
-    document.getElementById('videojs-wrapper').children[0].classList.remove('vjs-peertube-skin', 'vjs-has-starte');
-    vjsPlayer = videojs(document.querySelector('.video-js video'));
+    document.getElementById('videojs-wrapper').children[0].classList.remove('vjs-peertube-skin');
+    // document.getElementById('videojs-wrapper').children[0].classList.add('vjs-default-skin');
+
+
+    // const videoParent = document.getElementById("vjs_video_3");
+    // const chil = videoParent.querySelectorAll(":scope > .vjs-control-bar");
+
+
+    vjsPlayer = videojs(document.querySelector('.video-js video'), {
+        autoplay: false,
+        posterImage: true,
+        controls: true,
+        forceCardboard: false
+    });
+
+    var polyfill = new WebXRPolyfill({
+        cardboard: true,
+        allowCardboardOnDesktop: true,
+    });
 
     vjsPlayer.mediainfo = vjsPlayer.mediainfo || {};
+
+    console.log('vjsPlayer :>> ', vjsPlayer);
+
 
     if (video.tags.includes('360:180')) {
         vjsPlayer.mediainfo.projection = '180';
@@ -137,12 +217,18 @@ var goForVrPlayer = function(notifier, video) {
         vjsPlayer.mediainfo.projection = 'EAC_LR';
     }
 
-    vjsPlayer.xr({ projection: 'AUTO', debug: true, forceCardboard: true });
+    // vjsPlayer.xr({ projection: 'AUTO', debug: true, forceCardboard: true });
+    vjsPlayer.xr({ projection: 'AUTO' });
+
+
 
     vjsPlayer.xr().on('initialized', () => {
         vjsPlayer.xr().projection = vjsPlayer.mediainfo.projection;
-        console.log('switch to videojs-xr player with ' + vjsPlayer.mediainfo.projection + ' projection');
+        // vjsPlayer.xr({projection: '360'});
+        console.log('"xr" in navigator :>> ', "xr" in navigator);
+        console.log('trying to switch projection type to : ' + vjsPlayer.mediainfo.projection);
         notifier.success('Switched to 360° video player');
+
     });
 
 }
